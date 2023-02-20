ec13ddd90a57dc4b8da0acea94b5e4ca27db7bc9
zaki031 <115791170+zaki031@users.noreply.github.com>
2023-02-20 23:24:40 +0100

    signup form layout

--- pages/signin.js
+++ pages/signin.js
@@ -2,6 +2,7 @@ import Navbar from "../components/navbar";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { firebaseApp, auth } from "../src/config/firebase.config";
 import async from "@firebase/util";
+import Head from "next/head";
 const signin = () => {
   const provider = new GoogleAuthProvider();
   const signIn =  async () => {
@@ -9,10 +10,38 @@ const signin = () => {
   };
   return (
     <>
+    <Head>
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css"/>
+    </Head>
       <Navbar />
-        <button className="p-3 text-white bg-main rounded-xl" onClick={() => signIn()}>
+      <div className=" form_contain">
+        <br /><br />
+      <form className="p-4 m-auto border-2 border-main rounded-md  max-w-[700px] " action="">
+        <label htmlFor="email">Enter your email :</label>
+        <input type="email" placeholder="Enter your email" name="email" className="w-full py-3 pl-3 border-3 border-main" />
+        <br />
+        <br />
+        <label htmlFor="pass">Enter your password :</label>
+
+        <input type="password" name="pass" id="" placeholder="dont show your password to anyone" className="w-full py-3 pl-3 border-3 border-main" />
+     <br /><br />
+     <button type="submit" className="w-full p-3 text-white rounded-md bg-main">Create your account</button>
+    <br /><br />
+    <div className="w-full text-center or">
+            <h5 className="m-auto text-gray-400">Or</h5>
+
+    </div>
+    <br />
+     <button className="w-full p-3 text-white rounded-md bg-main" onClick={() => signIn()}>
+     <i className=" uil uil-google"></i>
+     &nbsp; &nbsp; 
           Sign in with google
         </button>
+      </form>
+      </div>
+
+     
+ 
     </>
   );
 };
